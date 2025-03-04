4aeaa3075aa39286ae4edd72344849921d97179c
Taouil-abdo <abdotaouil03@gmail.com>
2025-03-04 14:55:10 +0000

    add CongerController and hierarchyController with basic resource methods

---
+++ app/Http/Controllers/CongerController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conger;
+use App\Http\Requests\StoreCongerRequest;
+use App\Http\Requests\UpdateCongerRequest;
+
+class CongerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+        
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCongerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Conger $conger)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Conger $conger)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCongerRequest $request, Conger $conger)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Conger $conger)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/hierarchyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class hierarchyController extends Controller
+{
+    
+
+    public function index()
+    {
+        return view('hierarchy');
+    }
+
+}
